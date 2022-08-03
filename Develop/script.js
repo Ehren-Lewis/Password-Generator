14ef57d1ea58f49ce789930a9b8e709a9f1d7e0d
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-08-03 14:08:47 -0500

    Got all the form itself as well as all of the form input elements stored in varaiables to access for later

--- Develop/script.js
+++ Develop/script.js
@@ -8,8 +8,17 @@ const specialChar = "@#$&%!~";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+// Puts every form element into a variable
+const form = document.querySelector("#passwordForm");
+
+// Gathers all of the input elements from the for
+const formElements = Array.from(form.querySelectorAll("input"));
+
+
+console.log(formElements);
 
   passwordText.value = password;
 
