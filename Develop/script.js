e0bb47d9cbc9aa46aaf00ae30785dd9d1de86afa
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-08-03 15:07:25 -0500

    created the function for password generation and showing it on the screen, there was an issue with using the params as an array, had to switch it to an object for it to work. There's stil an issue if they don't check any checkboxes, as well as the html structure isn't user friendly.

--- Develop/script.js
+++ Develop/script.js
@@ -6,9 +6,37 @@ const upperCase = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numeric = '0123456789';
 const specialChar = "@#$&%!~";
 
+const params = {lowerCase, upperCase, numeric, specialChar};
+
+const createPassword = (formParams, passParams) => {
+  var passLength = 0;
+  var returnPassword = '';
+  var pickFromString = '';
+
+  for (var criteria of formParams) {
+    if (criteria[0] == 'length') {
+      passLength = Number(criteria[1]);
+      continue;
+    }
+
+    if (criteria[1] == "true") {
+      pickFromString += params[criteria[0]];
+    }
+  }
+
+  for (let i = 0; i < passLength; i++) {
+    returnPassword += pickFromString[Math.floor(Math.random() * pickFromString.length)];
+  }
+
+  var passwordText = document.querySelector("#password");
+  passwordText.innerHTML = returnPassword;
+
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
 // Puts every form element into a variable
@@ -17,10 +45,30 @@ const form = document.querySelector("#passwordForm");
 // Gathers all of the input elements from the for
 const formElements = Array.from(form.querySelectorAll("input"));
 
+const testForm = new FormData;
 
-console.log(formElements);
+formElements.forEach( ele => {
+  if (ele.type ==="number" ) {
+    if (ele.value.length < 1) {
+      alert("Please enter a number");
+      return;
+    }
 
-  passwordText.value = password;
+    if (ele.valueAsNumber > 128 || ele.valueAsNumber < 8) {
+      alert("Password either too long or too short");
+      return;
+    }
+
+    testForm.append(ele.id, ele.valueAsNumber);
+  } 
+
+  if (ele.type === "checkbox") {
+    testForm.append(ele.id, ele.checked);
+  }
+})
+
+  let new_arr = testForm.entries();
+createPassword(new_arr, params);
 
 }
 
